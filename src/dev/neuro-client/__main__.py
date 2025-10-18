80f96f3249b3367005f62d865bf94d7ee9adc5ab
Nakurity <nakurity@nakashireyumi.com>
2025-10-18 14:31:59 +0700

    Update __main__.py

--- src/dev/neuro-client/__main__.py
+++ src/dev/neuro-client/__main__.py
@@ -1,1 +1,24 @@
 # todo: make this export and initialoze the action schema for the neuro-client
+import os
+import importlib
+
+
+def load_actions(game_name):
+  # Load all Action definitions dynamically
+  actions = []
+  for file in os.listdir(ACTIONS_DIR):
+      if not file.endswith(".py") or file.startswith("__"):
+          continue
+
+      file_path = os.path.join(ACTIONS_DIR, file)
+      spec = importlib.util.spec_from_file_location(file[:-3], file_path)
+      module = importlib.util.module_from_spec(spec)
+      spec.loader.exec_module(module)
+
+      if hasattr(module, "schema"):
+          act = module.schema()
+          check_action(act)
+          actions.append(act)
+          print(f"[ACTION] Loaded: {act.name}")
+
+return actions
