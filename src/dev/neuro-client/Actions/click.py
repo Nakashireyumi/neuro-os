3e059e5d28375479c504b31a8d3814fa403eb138
Nakurity <nakurity@nakashireyumi.com>
2025-10-18 14:10:14 +0700

    Update click.py

--- src/dev/neuro-client/Actions/click.py
+++ src/dev/neuro-client/Actions/click.py
@@ -1,14 +1,16 @@
 from neuro_api.command import Action
 
-action = Action(
-    name="click",
-    description="If coordinates is provided, move the mouse to that area, then click",
-    schema={
-        "type": "object",
-        "properties": {
-            "coordinates": {"type": "integer", "x": 0, "y": 0},
-            "button": {"type": "string", "button": "left"}
-        },
-        "required": ["button"]
-    }
-)
+class Click:
+    def schema():
+       Action(
+         name="click",
+         description="If coordinates is provided, move the mouse to that area, then click",
+         schema={
+           "type": "object",
+           "properties": {
+               "coordinates": {"type": "integer", "x": 0, "y": 0},
+               "button": {"type": "string", "button": "left"}
+           },
+          "required": ["button"]
+         }
+      )
