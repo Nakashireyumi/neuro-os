55cc3c8c5d2ea0223e652047ecf668a83dfa5d0f
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-11-04 16:47:55 +0700

    Initialize Neuro Desktop Plugins System
    Added a docstring and imports for the plugin system.

--- neuro-desktop/src/plugins/__init__.py
+++ neuro-desktop/src/plugins/__init__.py
@@ -1,1 +1,18 @@
-# Plugins package for Neuro-OS
+# src/plugins/__init__.py
+"""Neuro Desktop Plugins System"""
+
+from .plugin_manager import (
+    PluginManager,
+    PluginManifest,
+    Plugin,
+    PluginContext,
+    get_plugin_manager
+)
+
+__all__ = [
+    'PluginManager',
+    'PluginManifest',
+    'Plugin',
+    'PluginContext',
+    'get_plugin_manager'
+]# Plugins package for Neuro-OS
